2fa649f26a9ff603638b453b6b8846d9e9ca3ee3
ezitounioussama <zitounioussama95@gmail.com>
2023-06-21 01:43:34 -0700

    adding multiple files like pop, push ...

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing just void
+  *@head: stack head
+  *@counter: line_number
+  *Return: nothing to return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
